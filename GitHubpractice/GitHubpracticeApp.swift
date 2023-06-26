03b42cd462eb0694a6c9b32199db009224bd04f2
Nicole <nicolenban@gmail.com>
2023-06-26 10:07:33 -0400

    Initial Commit

---
+++ GitHubpractice/GitHubpracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubpracticeApp.swift
+//  GitHubpractice
+//
+//  Created by Nunu Ban on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubpracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
